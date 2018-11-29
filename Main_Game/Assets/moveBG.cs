5ca61e51e20f3ed140fbc3f00a24eed054899595
norah <alnoor0416@gmail.com>
2018-11-29 16:21:37 +0300

    animation

--- Main_Game/Assets/moveBG.cs
+++ Main_Game/Assets/moveBG.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class moveBG : MonoBehaviour {
-
+	Material objectMaterial;
+	Vector2 offset;
+	public int Xv, Yv;
+	public float wight;
+	private void Awake () {
+		objectMaterial = GetComponent<Renderer>().material;
+	}
 	// Use this for initialization
 	void Start () {
-		
+		offset = new Vector2 (Xv, Yv);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		objectMaterial.mainTextureOffset += offset * Time.deltaTime * wight ;
 	}
 }
---
+++ Main_Game/Assets/playerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float speed,jumpForce,moveInput;
+	private Rigidbody2D rB;
+
+	private bool grounded;
+	public Transform groundCheck;
+
+
+	 
+	// Use this for initialization
+	void Start () {
+		rB = GetComponent <Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Jump(){
+
+	}
+
+	void Scroll(){
+
+	}
+}
